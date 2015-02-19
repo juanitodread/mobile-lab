3a414891ad4552ca830950cb95503b77df56d89b
Juan Sandoval <juanitodread@gmail.com>
2015-02-18 23:27:10 -0600

    Add GeoQuiz fixed GeoQuiz project

---
+++ android/GeoQuiz/src/org/juanitodread/mobile/android/geoquiz/QuizActivity.java
@@ -0,0 +1,89 @@
+/**
+ * GeoQuiz
+ *
+ * Copyright 2015 juanitodread
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.juanitodread.mobile.android.geoquiz;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * This is the main activity
+ *
+ * @author juanitodread
+ * @version 1.0
+ * 
+ * Feb 18, 2015
+ */
+public class QuizActivity extends ActionBarActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_quiz );
+
+        mTrueButton = ( Button ) findViewById( R.id.true_button );
+        mTrueButton.setOnClickListener( new View.OnClickListener( ) {
+            @Override
+            public void onClick( View v ) {
+                Toast.makeText( QuizActivity.this,
+                                R.string.correct_toast,
+                                Toast.LENGTH_SHORT ).show( );
+            }
+        } );
+
+        mFalseButton = ( Button ) findViewById( R.id.false_button );
+        mFalseButton.setOnClickListener( new View.OnClickListener( ) {
+            /* (non-Javadoc)
+             * @see android.view.View.OnClickListener#onClick(android.view.View)
+             */
+            @Override
+            public void onClick( View v ) {
+                Toast.makeText( QuizActivity.this,
+                                R.string.incorrect_toast,
+                                Toast.LENGTH_SHORT ).show( );
+            }
+        } );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu ) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater( ).inflate( R.menu.quiz, menu );
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item ) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId( );
+        if ( id == R.id.action_settings ) {
+            return true;
+        }
+        return super.onOptionsItemSelected( item );
+    }
+}
